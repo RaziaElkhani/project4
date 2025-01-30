1c6164edd96d8e06f8cbd9c269e16bf0a1876bf8
elkha <elkha@10.0.0.142>
2025-01-29 18:06:40 -0800

    adding variable

--- src/demo1/DemoTest4.java
+++ src/demo1/DemoTest4.java
@@ -3,5 +3,12 @@ package demo1;
 public class DemoTest4 {
 public static void main(String[] args) {
 	System.out.println("Java is Fun");
+	int a=20;
+	int b=30;
+	System.out.println(a/b);
 }
+
+
+	
 }
+
